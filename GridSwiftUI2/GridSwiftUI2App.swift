f47f5c19631290960d454698dbcf2894be027c10
wanhar <wanhar@sehatq.com>
2020-06-26 22:45:41 +0700

    Initial Commit

---
+++ GridSwiftUI2/GridSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  GridSwiftUI2App.swift
+//  GridSwiftUI2
+//
+//  Created by Wanhar on 26/06/20.
+//
+
+import SwiftUI
+
+@main
+struct GridSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
